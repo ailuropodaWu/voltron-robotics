460dd84c04f09282523d8ce009361eaad6b7c568
Chuan-Yu Wu <patrick050292@gmail.com>
2023-07-18 16:31:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from torchvision.io import read_image
 from voltron import instantiate_extractor, load
 
 # Load a frozen Voltron (V-Cond) model & configure a vector extractor
-vcond, preprocess = load("v-cond", device="cpu", freeze=True)
+vcond, preprocess = load("v-cond", device="cuda", freeze=True)
 vector_extractor = instantiate_extractor(vcond)()
 
 # Obtain & Preprocess an image =>> can be from a dataset, or camera on a robot, etc.
 #   => Feel free to add any language if you have it (Voltron models work either way!)
-img = preprocess(read_image("examples/verification/img/peel-carrot-initial.png"))[None, ...].to("cpu")
+img = preprocess(read_image("examples/img/peel-carrot-initial.png"))[None, ...].to("cuda")
 lang = ["peeling a carrot"]
 
 # Extract both multimodal AND vision-only embeddings!
