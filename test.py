3b48bab9975170cd4bfec96b1a8380e9e62a9308
Chuan-Yu Wu <patrick050292@gmail.com>
2023-07-22 22:35:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -17,3 +17,4 @@ visual_embeddings = vcond(img, mode="visual")
 # Use the `vector_extractor` to output dense vector representations for downstream applications!
 #   => Pass this representation to model of your choice (object detector, control policy, etc.)
 representation = vector_extractor(multimodal_embeddings)
+print(representation)
